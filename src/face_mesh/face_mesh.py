1d50f5e1331fd29a090a4d0971423f22a5f4a4b4
MTamon <mtamon.0044@gmail.com>
2022-10-11 15:01:41 +0900

    [add] face mesh base code

---
+++ src/face_mesh/face_mesh.py
@@ -0,0 +1,11 @@
+import mediapipe as mp
+
+class FaceMesh():
+    def __init__(self) -> None:
+        pass
+    
+    def __call__(self) -> None:
+        pass
+    
+    def apply_face_mesh(self, area: dict):
+        pass

37d9bc5205173aee9b22d653e571e13b2b955984
MTamon <mtamon.0044@gmail.com>
2022-12-04 10:01:06 +0900

    [fix] bug

--- src/building.py
+++ src/building.py
@@ -179,9 +179,10 @@ class CEJC_Builder:
         for _k in prime_pair:
             pair_sum = 1e-4
             for record in prime_pair[_k]:
-                pair_sum += record["score"]
+                pair_sum += record["score"] ** (1 + record["used"])
             for record in prime_pair[_k]:
-                record["score"] /= pair_sum
+                _score = record["score"] ** (1 + record["used"])
+                record["score"] = _score / pair_sum
 
         return prime_id
 
